7a76dfe6814df594f79fc7527dbf13ee2f3afaec
Vinit0 <vincent.de.vries.vinit0@gmail.com>
2018-06-15 15:04:54 +0200

    Change player mechanic

---
+++ Platformer E13/Assets/Standard Assets/2D/Scripts/PlayerShoot.cs
@@ -0,0 +1,51 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using System.Linq;
+
+public class PlayerShoot : MonoBehaviour {
+
+    PlatformerCharacter2D player;
+    public GameObject gun, bullet;
+    public float bulletSpeed;
+    private Vector3 mouseDir;
+    float cooldown = 0;
+    // Use this for initialization
+    void Start () {
+        player = GetComponent<PlatformerCharacter2D>();
+    }
+	
+	// Update is called once per frame
+	void Update () {
+        GetDirection();
+        if (Input.GetMouseButtonDown(1))
+        {
+            if(Cooldown(1f))
+                Shoot();
+        }
+	} 
+    private void Shoot()
+    {
+        var instance = Instantiate(bullet, gun.transform.position, Quaternion.identity);
+        instance.GetComponent<Rigidbody2D>().velocity = mouseDir * bulletSpeed;
+    }
+
+    private bool Cooldown(float seconds)
+    {
+        if (Time.time >= cooldown)
+        {
+            cooldown = Time.time + seconds;
+            return true;
+        }
+        return false;
+    }
+
+    private void GetDirection()
+    {
+        var mousePos = Camera.main.ScreenToWorldPoint(Input.mousePosition);
+
+        mouseDir = mousePos - transform.position;
+        mouseDir.z = 0.0f;
+        mouseDir = mouseDir.normalized;
+    }
+}
