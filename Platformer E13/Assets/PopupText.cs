40aee2d0a11c3f71bce5eb5806675adf59ad76cd
Vinit0 <vincent.de.vries.vinit0@gmail.com>
2019-01-15 12:03:32 +0100

    more changes

---
+++ Platformer E13/Assets/PopupText.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PopupText : MonoBehaviour {
+
+    public Animator animator;
+
+    private void OnTriggerStay(Collider other)
+    {
+        if(other.gameObject.tag == "Player")
+        {
+            animator.SetBool("IsOpen", true);
+        }
+        else
+        {
+            animator.SetBool("IsOpen", false);
+        }
+    }
+}
